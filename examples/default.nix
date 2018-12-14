08913cb3f583e8dd1fa114a1f3601f4919e1a267
Divam <dfordivam@gmail.com>
2018-12-15 06:54:00 +0900

    Add echarts thunk dir, fix default.nix

--- examples/default.nix
+++ examples/default.nix
@@ -2,13 +2,9 @@
 , iosSdkVersion ? "10.2"
 }:
 with import ./.obelisk/impl { inherit system iosSdkVersion; };
-project ./. ({ ... }: {
-  android.applicationId = "systems.obsidian.obelisk.examples.minimal";
-  android.displayName = "Obelisk Minimal Example";
-  ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
-  ios.bundleName = "Obelisk Minimal Example";
+project ./. ({ hackGet, ... }: {
   packages = {
-    echarts-jsdom = /home/divam/repos/reflex/echarts ;
-    reflex-dom-echarts = /home/divam/repos/reflex/dom-echarts ;
+    echarts-jsdom = hackGet ./echarts-jsdom;
+    reflex-dom-echarts = ../.;
   };
 })
